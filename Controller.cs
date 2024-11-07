cd6537b786f73d92de42c0c8cce9611345d77f53
elliotA1ders0n <130219411+elliotA1ders0n@users.noreply.github.com>
2024-11-07 13:48:34 +0100

    i was worki on the logiks

---
+++ Controller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Etterem
+{
+    internal class Controller
+    {
+        public LinkedList<Food> foods = new LinkedList<Food>();
+
+        public void fillFoods() { 
+        
+        }
+    }
+}
---
+++ Food.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Etterem
+{
+    internal class Food
+    {
+        private string name;
+        private string description;
+        private bool haveIngreedientsForOne;
+
+        private Food(string name, string description)
+        {
+            this.name = name;
+            this.description = description;
+            this.haveIngreedientsForOne = HasIngreedientsForOne(name);
+        }
+
+        public void fillFoods()
+        {
+
+        }
+
+
+
+
+        public bool HasIngreedientsForOne(string name)
+        {
+
+        }
+    }
+}
